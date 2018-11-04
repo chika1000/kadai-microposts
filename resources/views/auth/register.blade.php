c10fab43c93c9a951fdde341e346f12f228144e5
chika1000 <chika1000iwai@gmail.com>
2018-11-04 17:38:28 +0000

    bug fixed

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -21,7 +21,7 @@
 
                 <div class="form-group">
                     {!! Form::label('password', 'Password') !!}
-                    {!! Form::password('password', ['class' => 'form-cotrol']) !!}
+                    {!! Form::password('password', ['class' => 'form-control']) !!}
                 </div>
 
                 <div class="form-group">
